076059f3bb86eb05d8675671458d93659e978163
Maxime Monnier <maximemonnier39@gmail.com>
2022-12-07 16:17:56 +0400

    add closing connection

--- database/DBControler.php
+++ database/DBControler.php
@@ -12,13 +12,28 @@ protected $database = 'shopee';
 public $con = null;
 
 //call constructor
-public function __construct() {
+public function __construct()
+{
     //initialize connection property
     $this->con = mysqli_connect($this->host,$this->user,$this->password,$this->database);
     if($this->con->connect_error){
         echo "Fail".$this->con->connect_error;
     }
-} 
+}
+
+public function __destruct()
+{
+    $this->closeConnection();
+}
+
+//for msqli closing connection
+protected function closeConnection()
+{
+    if($this->con !=null){
+        $this->con->close();
+        $this->con = null;
+    }
+}
 
 }
 
