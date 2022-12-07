83747902f9ec51de93ddd47b04ceef26928be441
Maxime Monnier <maximemonnier39@gmail.com>
2022-12-07 17:19:47 +0400

    fetch data for top-sale section

--- Templates/_top-sale.php
+++ Templates/_top-sale.php
@@ -1,3 +1,7 @@
+<?php
+    $product_shuffle = $product->getData();
+?>
+        
         <!-- top sale -->
         <section id="top-sale">
             <div class="container py-5">
@@ -5,13 +9,14 @@
                 <hr>
                 <!-- Owl-caroussel -->
                 <div class="owl-carousel owl-theme">
+                    <?php foreach ($product_shuffle as $item) { ?>
                     <div class="item py-2">
                         <div class="product font-mont">
-                            <a href="#"><img src="./assets/products/1.png"
+                            <a href="#"><img src="<?php echo $item['item_image'] ?? "./assets/products/1.png"  ?>"
                                     alt="product1"
                                     class="img-fluid"></a>
                             <div class="text-center">
-                                <h6>Samsung Galaxie 21</h6>
+                                <h6><?php echo $item['item_name'] ?? "Unknow" ?></h6>
                                 <div class="rating text-warning font-size-12">
                                     <span><i class="fas fa-star"></i></span>
                                     <span><i class="fas fa-star"></i></span>
@@ -20,159 +25,13 @@
                                     <span><i class="far fa-star"></i></span>
                                 </div>
                                 <div class="price py-2">
-                                    <span>$152</span>
+                                    <span>$<?php echo $item['item_price'] ?? "Unknow" ?></span>
                                 </div>
                                 <button type="submit" class="btn btn-warning font-size-12">Ajouter au Panier</button>
                             </div>
                         </div>
                     </div>
-                    <div class="item py-2">
-                        <div class="product font-mont">
-                            <a href="#"><img src="./assets/products/3.png"
-                                    alt="product1"
-                                    class="img-fluid"></a>
-                            <div class="text-center">
-                                <h6>Readme Note 7</h6>
-                                <div class="rating text-warning font-size-12">
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="far fa-star"></i></span>
-                                </div>
-                                <div class="price py-2">
-                                    <span>$152</span>
-                                </div>
-                                <button type="submit" class="btn btn-warning font-size-12">Ajouter au Panier</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item py-2">
-                        <div class="product font-mont">
-                            <a href="#"><img src="./assets/products/3.png"
-                                    alt="product1"
-                                    class="img-fluid"></a>
-                            <div class="text-center">
-                                <h6>iphone7</h6>
-                                <div class="rating text-warning font-size-12">
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="far fa-star"></i></span>
-                                </div>
-                                <div class="price py-2">
-                                    <span>$152</span>
-                                </div>
-                                <button type="submit" class="btn btn-warning font-size-12">Ajouter au Panier</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item py-2">
-                        <div class="product font-mont">
-                            <a href="#"><img src="./assets/products/2.png"
-                                    alt="product1"
-                                    class="img-fluid"></a>
-                            <div class="text-center">
-                                <h6>Readme Note 7</h6>
-                                <div class="rating text-warning font-size-12">
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="far fa-star"></i></span>
-                                </div>
-                                <div class="price py-2">
-                                    <span>$152</span>
-                                </div>
-                                <button type="submit" class="btn btn-warning font-size-12">Ajouter au Panier</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item py-2">
-                        <div class="product font-mont">
-                            <a href="#"><img src="./assets/products/1.png"
-                                    alt="product1"
-                                    class="img-fluid"></a>
-                            <div class="text-center">
-                                <h6>Samsung Galaxie 21</h6>
-                                <div class="rating text-warning font-size-12">
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="far fa-star"></i></span>
-                                </div>
-                                <div class="price py-2">
-                                    <span>$152</span>
-                                </div>
-                                <button type="submit" class="btn btn-warning font-size-12">Ajouter au Panier</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item py-2">
-                        <div class="product font-mont">
-                            <a href="#"><img src="./assets/products/3.png"
-                                    alt="product1"
-                                    class="img-fluid"></a>
-                            <div class="text-center">
-                                <h6>Readme Note 7</h6>
-                                <div class="rating text-warning font-size-12">
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="far fa-star"></i></span>
-                                </div>
-                                <div class="price py-2">
-                                    <span>$152</span>
-                                </div>
-                                <button type="submit" class="btn btn-warning font-size-12">Ajouter au Panier</button>
-                            </div>
-                        </div>
-                    </div>
-                     <div class="item py-2">
-                        <div class="product font-mont">
-                            <a href="#"><img src="./assets/products/1.png"
-                                    alt="product1"
-                                    class="img-fluid"></a>
-                            <div class="text-center">
-                                <h6>Samsung Galaxie 21</h6>
-                                <div class="rating text-warning font-size-12">
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="far fa-star"></i></span>
-                                </div>
-                                <div class="price py-2">
-                                    <span>$152</span>
-                                </div>
-                                <button type="submit" class="btn btn-warning font-size-12">Ajouter au Panier</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item py-2">
-                        <div class="product font-mont">
-                            <a href="#"><img src="./assets/products/3.png"
-                                    alt="product1"
-                                    class="img-fluid"></a>
-                            <div class="text-center">
-                                <h6>Readme Note 7</h6>
-                                <div class="rating text-warning font-size-12">
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="fas fa-star"></i></span>
-                                    <span><i class="far fa-star"></i></span>
-                                </div>
-                                <div class="price py-2">
-                                    <span>$152</span>
-                                </div>
-                                <button type="submit" class="btn btn-warning font-size-12">Ajouter au Panier</button>
-                            </div>
-                        </div>
-                    </div>
+                    <?php } //closing foreach loupe ?>
                 </div>
                 <!-- !Owl-caroussel -->
             </div>
--- functions.php
+++ functions.php
@@ -2,5 +2,15 @@
  //require MySQL Connection
  require 'database/DBControler.php';
 
+  //require Product Class
+  require 'database/Product.php';
+
+
  //DBController object
  $db = new DBController();
+
+ //product object
+
+ $product = new Product($db);
+
+print_r($product->getData());
