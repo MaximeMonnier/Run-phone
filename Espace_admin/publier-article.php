3519405a6eb54b6ec0a1c4af8f0013bd3597ca27
Maxime Monnier <maximemonnier39@gmail.com>
2022-12-14 17:34:01 +0400

    creation d'un back office ajout d'un espace de bannissement membre + d'un espace de création article

--- Espace_admin/publier-article.php
+++ Espace_admin/publier-article.php
@@ -5,4 +5,33 @@ $bdd = new PDO ('mysql:host=localhost;dbname=espace_admin;','root','');
 if(!$_SESSION['mdp']){
     header('Location: connexion.php');
 }
+
+if(isset($_POST['envoi'])){
+
+    if(!empty($_POST['titre']) AND !empty($_POST['descritpion'])){
+
+    }else{
+        echo "Veuillez compléter tous les champs..."
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Publier un article</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" name="titre">
+        <br>
+        <textarea name="description"></textarea>
+        <br>
+        <input type="submit" name="envoi">
+    </form>
+    
+</body>
+</html>
